b221ce2bfcf497be36b56bee91f631ca1afc52b7
Tim <mideltim@gmail.com>
2025-07-17 21:53:51 +0800

    feat: add shuffle button

--- src/app/flashcards/page.tsx
+++ src/app/flashcards/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useContext, useState, useEffect } from "react";
+import { Shuffle } from "lucide-react";
 import { FlashcardContext } from "../context/FlashcardContext";
 import FlashcardComponent from "../components/Flashcard";
 
@@ -8,6 +9,7 @@ export default function FlashcardsPage() {
   const context = useContext(FlashcardContext);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isFlipped, setIsFlipped] = useState(false);
+  const [isFading, setIsFading] = useState(false);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -26,16 +28,33 @@ export default function FlashcardsPage() {
     return null;
   }
 
-  const { flashcards } = context;
+  const flashcards = context ? context.flashcards : [];
+  const setFlashcards = context ? context.setFlashcards : () => {};
   const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % flashcards.length);
-    setIsFlipped(false);
+    setIsFading(true);
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % flashcards.length);
+      setIsFlipped(false);
+      setIsFading(false);
+    }, 200); // Adjust duration as needed
   };
 
   const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? flashcards.length - 1 : prevIndex - 1
-    );
+    setIsFading(true);
+    setTimeout(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? flashcards.length - 1 : prevIndex - 1
+      );
+      setIsFlipped(false);
+      setIsFading(false);
+    }, 300); // Adjust duration as needed
+  };
+
+  const handleShuffle = () => {
+    if (flashcards.length === 0) return;
+    const shuffledFlashcards = [...flashcards].sort(() => Math.random() - 0.5);
+    setFlashcards(shuffledFlashcards);
+    setCurrentIndex(0);
     setIsFlipped(false);
   };
 
@@ -50,18 +69,30 @@ export default function FlashcardsPage() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center pt-10 py-20">
-      <div className="w-full max-w-2xl">
+    <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center relative pt-10 pb-20">
+      <div
+        className={`w-full max-w-2xl transition-opacity duration-300 ${
+          isFading ? "opacity-0" : "opacity-100"
+        }`}
+      >
         <FlashcardComponent
           flashcard={flashcards[currentIndex]}
           isFlipped={isFlipped}
           onClick={() => setIsFlipped(!isFlipped)}
         />
       </div>
+      <div className="fixed top-4 right-4">
+        <button
+          onClick={handleShuffle}
+          className="px-4 py-2 text-white rounded-md cursor-pointer hover:scale-125 transition-all focus:outline-none focus:ring-0"
+        >
+          <Shuffle className="w-9 h-9" />
+        </button>
+      </div>
       <div className="bg-gray-900/90 fixed bottom-0 left-0 right-0 p-4 flex items-center justify-center space-x-4">
         <button
           onClick={handlePrev}
-          className="px-4 py-2 bg-purple-600  text-white rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+          className="px-4 py-2 bg-purple-600 cursor-pointer text-white rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
         >
           &larr; Prev
         </button>
@@ -70,7 +101,7 @@ export default function FlashcardsPage() {
         </span>
         <button
           onClick={handleNext}
-          className="px-4 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+          className="px-4 py-2 bg-purple-600 cursor-pointer text-white rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
         >
           Next &rarr;
         </button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useContext } from "react";
+import { useState, useContext, useRef } from "react";
 import { useRouter } from "next/navigation";
 import * as mammoth from "mammoth";
 import { FlashcardContext } from "./context/FlashcardContext";
@@ -11,6 +11,7 @@ export default function Home() {
   const [dragged, setDragged] =
     useState<React.DragEvent<HTMLDivElement> | null>(null);
   const context = useContext(FlashcardContext);
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
   const router = useRouter();
 
   if (!context) {
@@ -95,13 +96,24 @@ export default function Home() {
         </h1>
         <div className="mt-8 space-y-6">
           <div
-            className={`mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-dashed rounded-md ${
+            className={`mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-dashed rounded-md transition-all cursor-pointer ${
               dragged ? "border-purple-500" : "border-gray-600"
-            }`}
+            } hover:border-purple-500`}
+            onClick={() => fileInputRef.current?.click()}
             onDragOver={handleDragOver}
             onDragLeave={handleDragLeave}
             onDrop={handleDrop}
           >
+            <input
+              ref={fileInputRef}
+              id="file-upload"
+              name="file-upload"
+              type="file"
+              className="hidden"
+              onChange={handleFileChange}
+              accept=".docx"
+            />
+
             <div className="space-y-1 text-center">
               <svg
                 className="mx-auto h-12 w-12 text-gray-500"
@@ -117,25 +129,18 @@ export default function Home() {
                   strokeLinejoin="round"
                 />
               </svg>
-              <div className="flex text-sm text-gray-400">
-                <label
-                  htmlFor="file-upload"
-                  className="relative cursor-pointer bg-gray-800 rounded-md font-medium text-purple-400 hover:text-purple-300 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-purple-500"
-                >
-                  <span>Upload a file</span>
-                  <input
-                    id="file-upload"
-                    name="file-upload"
-                    type="file"
-                    className="sr-only"
-                    onChange={handleFileChange}
-                    accept=".docx"
-                  />
-                </label>
+              <div className="flex text-sm text-gray-400 justify-center">
+                <span className="font-medium text-purple-400">
+                  Upload a file
+                </span>
                 <p className="pl-1">or drag and drop</p>
               </div>
-              <p className="text-xs text-gray-500">DOCX up to 10MB</p>
-              {file && <p className="text-sm text-gray-500">{file.name}</p>}
+              <p className="text-xs text-gray-500 mb-3">DOCX up to 10MB</p>
+              {file && (
+                <p className="text-sm text-gray-300 font-semibold">
+                  {file.name}
+                </p>
+              )}
             </div>
           </div>
           <div>
@@ -159,7 +164,7 @@ export default function Home() {
           <div>
             <button
               onClick={generateFlashcards}
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+              className="w-full flex justify-center cursor-pointer py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
             >
               Generate Flashcards
             </button>
